facb587259eaf1f88196282c94b4451bae6aa426
Joel Buchheim-Moore <joelbm24@gmail.com>
2020-01-05 21:38:37 -0800

    fixed another bug

--- lib/dapirRequest.dart
+++ lib/dapirRequest.dart
@@ -4,9 +4,9 @@ class DapirRequest {
     String verb;
     Map<String, String> header;
     String url;
-    Map<String, dynamic> body;
+    dynamic body;
 
-    DapirRequest({String verb, Map<String, String> header, String url, Map<String, dynamic> body}) {
+    DapirRequest({String verb, Map<String, String> header, String url, dynamic body}) {
         this.verb = verb;
         this.header = header;
         this.url = url;
